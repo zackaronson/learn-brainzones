4efd428476853f0636c02c8d2f076521c8505d59
Zack Aronson <aronson.zack@gmail.com>
2018-06-12 20:00:55 -0500

    File Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// Grab the hamburger menu
+
+var nav = document.querySelector('.nav-button');
+
+// grab mobile menu
+var menu = document.querySelector('.nav_mobile');
+
+nav.addEventListener('click', function(e){
+  if(menu.classList.contains('active')) {
+    // if the mobile menu has the 'active' class
+    // remove it
+    menu.classList.remove('active');
+
+  } else {
+    // else, add the active class
+    // this is a vanilla JS implementation of
+    // jQuery's .toggleClass();
+    menu.classList.add('active');
+  }
+});
+
+
+// if the mobile menu is left active
+// and the screen is made larger, then smaller,
+// let's remove the mobile menu's active class
+// to ensure a fresh start
+window.onresize = function(){
+  if(window.innerWidth > 420) {
+    menu.classList.remove('active');
+  }
+};
