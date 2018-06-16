d5bf452ee1637d3688471cdd9aa10165baf75df5
Zack A <aronson.zack@gmail.com>
2018-06-16 14:05:46 -0500

    Menu class is working
    - got javascript menu working with adding class
    - the transform selector is moving the menu down
    - still need to figure out structure for animation

--- app.js
+++ app.js
@@ -1,31 +1,14 @@
-// Grab the hamburger menu
+var button = document.querySelector('.menu-button');
 
-var nav = document.querySelector('.nav-button');
+var item = document.querySelector('.item-list');
 
-// grab mobile menu
-var menu = document.querySelector('.nav-mobile');
+button.addEventListener('click', function(){
+  if (item.classList.contains('active')) {
+    item.classList.remove('active');
 
-nav.addEventListener('click', function(){
-  if(menu.classList.contains('active')) {
-    // if the mobile menu has the 'active' class
-    // remove it
-    menu.classList.remove('active');
-
-  } else {
-    // else, add the active class
-    // this is a vanilla JS implementation of
-    // jQuery's .toggleClass();
-    menu.classList.add('active');
   }
+  else {
+    item.classList.add('active');
+  }
+
 });
-
-
-// if the mobile menu is left active
-// and the screen is made larger, then smaller,
-// let's remove the mobile menu's active class
-// to ensure a fresh start
-window.onresize = function(){
-  if(window.innerWidth > 420) {
-    menu.classList.remove('active');
-  }
-};
